57249c8f6013a2f35b6971076d308edeb77dab3d
sacsand <c4c5nf3@gmail.com>
2016-07-07 15:20:32 +0530

    10th Icon added

---
+++ src/test/Trasparencia.java
@@ -0,0 +1,26 @@
+package test;
+
+
+import com.sun.awt.AWTUtilities;
+import javax.swing.JFrame;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sachintha
+ */
+class Trasparencia {
+
+   
+    void TransCompFrame(NewJFrame aThis) {
+        aThis.setUndecorated(true);
+        AWTUtilities.setWindowOpaque(aThis,false) ;
+    }
+    
+}
